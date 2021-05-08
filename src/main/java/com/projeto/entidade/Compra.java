465d13737ea3558d2039a7ce4f3959e9133fe80b
Ruan <Ruan@DESKTOP-OML89A0>
2021-05-08 16:15:33 -0300

    Fiz Algumas Alterações em Compras para poder fazer o DAO dela.

--- src/main/java/com/projeto/entidade/Compra.java
+++ src/main/java/com/projeto/entidade/Compra.java
@@ -1,6 +1,8 @@
 package com.projeto.entidade;
 
+import java.sql.Date;
 import java.util.ArrayList;
+
 
 /**
  *
@@ -9,34 +11,34 @@ import java.util.ArrayList;
 public class Compra{
     
     private final int id;
-    private Cliente cliente;
-    private Funcionario funcionario;
+    private int Id_cliente;
+    private Date data;
+    private int Id_funcionario;
     private double vendaTotal = 0;
     private ArrayList<Item> Items = new ArrayList<>();
 
     @Override
     public String toString(){
-        return String.format("CPF:%s\nNumero do Pedido:%1d\nAtendimento:%1d\nValor todal do pedido:$%.2f", this.cliente, this.id, funcionario.getId(), getVendaTotal());
+        return String.format("CPF:%s\nNumero do Pedido:%1d\nAtendimento:%1d\nValor todal do pedido:$%.2f", this.Id_cliente, this.id, this.Id_funcionario, getVendaTotal());
     }
     
     public Compra(int ID) {
         this.id = ID;
     }
-
-    public Compra(int id, Funcionario funcionario) {
+  
+    public Compra(int id, int id_cliente, int id_funcionario, double VendaTotal) {
         this.id = id;
-        this.funcionario = funcionario;
-    }
-
-    public Compra(int id, Cliente cliente) {
-        this.id = id;
-        this.cliente = cliente;
+        this.Id_cliente = id_cliente;
+        this.Id_funcionario = id_funcionario;
+        this.vendaTotal = VendaTotal;
     }
     
-    public Compra(int id, Cliente cliente, Funcionario funcionario) {
+    public Compra(int id, int Id_cliente, int Id_funcionario, double VendaTotal, Date Data) {
         this.id = id;
-        this.cliente = cliente;
-        this.funcionario = funcionario;
+        this.Id_cliente = Id_cliente;
+        this.Id_funcionario = Id_funcionario;
+        this.vendaTotal = VendaTotal;
+        this.data = Data;
     }
     
     public void addAllItens(ArrayList<Item> items){
